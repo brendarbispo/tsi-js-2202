3f4d26ba8223f0d563a9d4f44d24a32a9fecdf98
BRENDA RODRIGUES BISPO <brenda.rbispo@senacsp.edu.br>
2022-09-16 22:14:55 -0300

    DOM continuação

--- DOM/ex2.js
+++ DOM/ex2.js
@@ -1,17 +1,30 @@
 let btnCalcular = document.getElementById('calcular');
 
 btnCalcular.addEventListener('click', function(evento){
-
-    //previnir o evento padrão
     evento.preventDefault();
-    console.log(evento);
-    
-    //OBTER OS VALORES DE SERVIÇO E PRAZO
+        let serv = document.getElementById('servico');
+        let prazo = document.getElementById('prazo');
+        let res = document.getElementById('resultado');
+         
+         //recuperar os dados da matriz
+         let preco = calculaPreco(serv.value, prazo.value);
+         
 
-    
+        res.innerText = `O preço é ${preco}`;
+
+        //ver dados do evento (click) no console::
+        console.log(evento);
+
+        //Obter os valores do serviço e prazo::        
 });
 
+function calculaPreco(servico, prazo){
+        let tabela = [ ['3k', '2k', '1k'],
+                    ['4k', '3k', '2k'],
+                    ['Não faz','Não faz','3k']];
 
+                return tabela [servico][prazo];
+}
 
 
 /*
---
+++ ex3.js
@@ -0,0 +1,24 @@
+let elemento = document;
+let e;
+//e = document.body.firstElementChild;
+//e = document.body.firstElementChild.firstElementChild;
+//e = document.doctype.forms[0];
+//e = document.doctype.forms[0][1];
+//e = document.doctype.scripts;
+//e = document.doctype.script[2].getAttribute('src');
+
+
+//1 e = document.body.lastElementChild;
+//2 e = document.body.firstElementChild.firstChild;
+//3 e = document.body.firstElementChild.firstChild;
+//4 e = document.getElementsByClassName('card-action');
+//5 e = document.getelementById('task-title');
+
+//e = document.querySelector('span');
+//e = document.querySelector('div .row');
+e = document.querySelector('input#task');
+
+
+
+
+console.log(e);
