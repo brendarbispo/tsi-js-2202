f4e15b7ee17d4432fc4efef22373cd1fb0c3fa9a
BRENDA RODRIGUES BISPO <brenda.rbispo@senacsp.edu.br>
2022-11-18 23:08:11 -0200

    finalização ipca

--- graficos/ipca.js
+++ graficos/ipca.js
@@ -1,79 +1,76 @@
 document.querySelector('button').addEventListener('click', mostraIpca);
+ 
+ function mostraIpca(evento){
+     evento.preventDefault();
+ 
+    document.querySelector('#ipca').innerHTML = '';
+     const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101';
+    }
+ 
+     fetch(url).then(function(retorno){
+ 
+         return retorno.text();
+ 
+     }).then(function(stringJson){
+ 
+            let anoUsuario = document.querySelector('#ano').value;
 
-function mostraIpca(evento){
+         const indices = JSON.parse(stringJson);
+ 
+        //////  Fazer em casa! //////
+        //colocar os dados recuperados da API 
+        //no gráfico
+        let indice;
+        let ipca = [];
+     
+        indices.forEach( function( mes ){
 
-    evento.preventDefault();
+            anoString = mes.data.substring(6, 10);
+            mesString = mes.data.substring(3, 5);
+            anoMesString = anoString + '-' + mesString;
 
-       const url = 'api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101'
+            if( parseInt(anoString) != anoUsuario && 
+                anoUsuario != ""){
+                return;
+            }
+        
 
-       fetch(url).then(function(retorno){
+            indice = parseFloat(mes.valor);
 
-            return retorn.text();
-
-        }).then(function(stringJson){
-
-           const indices = JSON.parse(stringJson);
-
-           // Fazer em casa! //
-           //Colocar os dados recuperados da API
-           //No gráfico
-
-            jsonPraMorris = { 
-                element: 'ipca',
-                // Dados do gráfico
-
-                data: [
-
-                  { year: '2008', value: 20 },
-
-                  { year: '2009', value: 10 },
-
-                  { year: '2010', value: 5 },
-
-                  { year: '2011', value: 5 },
-
-                  { year: '2012', value: 20 },
-
-                  { year: '2013', value: 25 },
-
-                  { year: '2014', value: 7 }
-
-                ],
-
-            
-
-                // O nome do eixo com os valores de X
-
-                xkey: 'year',
-
-                
-
-                // Uma lista de nomes dos atributos de dados contidos em Y
-
-                ykeys: ['value'],
-
-            
-
-                // Rótulos para os índices -- aparece quando passa o mouse por cima
-
-                labels: ['Value']};
-
-
-
-            new Morris.line(jsonPraMorris)
-
-
-
-        }).catch(function(){
-
-
-
-            alert('API do Banco Central fora do ar');
-
-
-
+            ipca.push({ month: anoMesString, value: indice});
         });
+    
 
+        if(ipca.length == 0){
 
-
-}
+            alert(`Não há dados para o ano ${anoUsuario}`);
+        }
+ 
+         jsonParaMorris = {
+                             element: 'ipca',
+                                // Dados do gráfico
+                            data: [
+                                { year: '2008', value: 20 },
+                                { year: '2009', value: 10 },
+                                { year: '2010', value: 5 },
+                                { year: '2011', value: 5 },
+                                { year: '2012', value: 20 },
+                                { year: '2013', value: 25 },
+                                { year: '2014', value: 7 }
+                            ],
+                        
+                            // Dados do gráfico
+                            data: ipca,
+                             // O nome do eixo com os valores de X
+                            xkey: 'year',
+                            
+                            xkey: 'month',
+                             // Uma lista de nomes dos atributos de dados contidos em Y
+                             ykeys: ['value'],
+                        
+                             // Rótulos para os índices -- aparece quando passa o mouse por cima
+                            labels: ['Value'],
+                            labels: ['IPCA']
+                         };
+ 
+         new Morris.Line(jsonParaMorris); 
